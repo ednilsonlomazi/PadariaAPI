ab6fb0d0f9a7f6791e979c94374b1e1bc32df7dd
ednilson-lomazi <65460604+ednilsonlomazi@users.noreply.github.com>
2024-05-02 21:49:30 -0300

    inicio-web-api

---
+++ WebAPI/Controllers/Controller.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/ola-mundo")]
+    [ApiController]
+    public class MeuController : ControllerBase
+    {
+
+    }
+}
---
+++ WebAPI/Entity/TabProduto.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Entity {
+    public class TabProduto
+    {
+        public Guid Id {get; set;}
+
+    }
+}
